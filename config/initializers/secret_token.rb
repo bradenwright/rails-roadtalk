6165f9e1adf2da1ece88e0dada11f2f39547f9c7
Braden Wright <braden.m.wright@gmail.com>
2014-04-05 14:28:37 -0500

    example app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Roadtalk::Application.config.secret_key_base = '13ac27d3024c5e8d8c05afa56621e8f39d30231da93e2b97d5616ea0d205e016c426f9798acb2b230d5d50ba98c85862895041a558567030af505d93f9782f69'
